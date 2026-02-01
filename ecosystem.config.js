8e585dd695f88c90a7236a393894a1b07de834f4
pengcheng <ptcpeng@tungee.com>
2026-02-01 13:30:14 +0800

    add ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'ink-and-code',
-      script: 'node_modules/.bin/next',
+      script: 'node_modules/next/dist/bin/next',
       args: 'start -p 80',
-      cwd: './',
+      cwd: '/ptc/ink-and-code',
       instances: 1,
       exec_mode: 'fork',
       autorestart: true,
